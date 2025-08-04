1af203a42c468f5679a778b999de91579cd9a414
mekseb <meksebmehari99@gmail.com>
2025-08-04 16:18:35 +0200

    QR code Generator

---
+++ index.js
@@ -0,0 +1,23 @@
+
+
+import inquirer from 'inquirer';
+
+inquirer
+  .prompt([
+    {message: "your url:",  
+    name : "url"
+    /* Pass your questions in here */
+  }])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+
+    console.log(answers)
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+
